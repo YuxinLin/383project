140872391c614c11b7725c658910ce3a2f0a40b3
YuxinLin <32252366+YuxinLin@users.noreply.github.com>
2018-04-26 22:20:38 -0700

    Update process_producer-04:26:22_16-yuxin.h

--- process_producer-04:26:22_16-yuxin.h
+++ process_producer-04:26:22_16-yuxin.h
@@ -1,35 +1,34 @@
 #ifndef PROCESS_PRODUCER
 #define PROCESS_PRODUCER
 
-#define NUM_PROCESS 50 //determine the number of process
-#define MAX_QUANTA 100 //determine the maximum arrival_time
+#define NUM_PROCESS 26 //26 max possible number
+#define MAX_QUANTA 100
 #define CHAR_ARRAYMAX 150
 
-/* Structure: Process 
-*For each simulated process, randomly generate:
-*An arrival time: a float value from 0 through 99 (measured in quanta).
-*An expected total run time: a float value from 0.1 through 10 quanta.
-*A priority: integer 1, 2, 3, or 4 (1 is highest)
+/* Struct: process
+*  struct created in accordance to Ezzat's specifications
 */
-
-struct process
-{
-	char name; // name from A to Z
-	int priority; // 1 to 4, 1 is highest
-	float arrival_time; // 0 to 99 
-	float service_time; // 0.1 to 10
-	int actual_start_time; //when the process is contacted by CPU for first time
-	int end_time; //process finishes
-        float initend_time;
+struct process{
+	float arrival_time; // time process arrives in queue
+	float service_time; // how much time to finish process
+	int priority; // priority 1 - 4, 1 highest priority for priority based scheduling
+	char name; // name A-Z or 0 - 9
+	int actual_start_time; // when the process is contacted by CPU for first time
+	int end_time; // process finishes
+	int initend_time; // initial end time, used for SRT (AG), it will be changed during the running SRT
 };
 
-void CreateProcesses(struct process** process_list);
-/* Function: CreateNewProcess
-*Constructs an array of struct process.
-*The parameter process_list is a pointer that will point to the created list
-*/
-
+/**
+ * [PrintProcessList - prints all process attributes except end time and actual start time]
+ * @param a [list to print]
+ */
 void PrintProcessList(struct process* a);
-//Print all process properties includes name, ariority, arrival_time and aervice_time
+
+/**
+ * [CreateProcesses pass in a pointer, this function constructs an array of struct process. How many process is determined by the macro NUM_PROCESS and the max arrival time is determined by MAX_QUANTA]
+ * @param process_list [pointer that will point to the created list]
+ */
+void CreateProcesses(struct process** process_list);
+//void SortProcesses(struct process** process_list);
 
 #endif
